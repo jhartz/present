bf5d4f3692a866995623fd50da47e9e31220eb48
Jake Hartz <jhartz@outlook.com>
2016-07-29 22:58:43 -0400

    Prototypes for C methods

--- include/timestamp.h
+++ include/timestamp.h
@@ -87,7 +87,82 @@ private:
 extern "C" {
 #endif
 
+struct Timestamp
+Timestamp_create_from_time_t(const time_t timestamp);
 
+
+struct Timestamp
+Timestamp_create_from_struct_tm_utc(const struct tm tm);
+
+struct Timestamp
+Timestamp_create_from_struct_tm(const struct tm tm, const struct TimeDelta * const timeZoneOffset);
+
+struct Timestamp
+Timestamp_create_from_struct_tm_local(const struct tm tm);
+
+
+struct Timestamp
+Timestamp_create_utc(const struct Date * const date, const struct ClockTime * const clockTime);
+
+struct Timestamp
+Timestamp_create(const struct Date * const date, const struct ClockTime * const clockTime, const struct TimeDelta * const timeZoneOffset);
+
+struct Timestamp
+Timestamp_create_local(const struct Date * const date, const struct ClockTime * const clockTime);
+
+
+struct Timestamp
+Timestamp_now();
+
+struct Timestamp
+Timestamp_epoch();
+
+
+struct Date
+Timestamp_get_date_utc(const struct Timestamp * const self);
+
+struct Date
+Timestamp_get_date(const struct Timestamp * const self, const struct TimeDelta * const timeZoneOffset);
+
+struct Date
+Timestamp_get_date_local(const struct Timestamp * const self);
+
+
+struct ClockTime
+Timestamp_get_clock_time_utc(const struct Timestamp * const self);
+
+struct ClockTime
+Timestamp_get_clock_time(const struct Timestamp * const self, const struct TimeDelta * const timeZoneOffset);
+
+struct ClockTime
+Timestamp_get_clock_time_local(const struct Timestamp * const self);
+
+void
+Timestamp_add_time_delta(struct Timestamp * const self, const struct TimeDelta * const delta);
+
+void
+Timestamp_add_month_delta(struct Timestamp * const self, const struct MonthDelta * const delta);
+
+void
+Timestamp_subtract_time_delta(struct Timestamp * const self, const struct TimeDelta * const delta);
+
+void
+Timestamp_subtract_month_delta(struct Timestamp * const self, const struct TimeDelta * const delta);
+
+bool
+Timestamp_equal(const struct Timestamp * const, const struct Timestamp * const);
+
+bool
+Timestamp_less_than(const struct Timestamp * const, const struct Timestamp * const);
+
+bool
+Timestamp_less_than_or_equal(const struct Timestamp * const, const struct Timestamp * const);
+
+bool
+Timestamp_greater_than(const struct Timestamp * const, const struct Timestamp * const);
+
+bool
+Timestamp_greater_than_or_equal(const struct Timestamp * const, const struct Timestamp * const);
 
 #ifdef __cplusplus
 }
