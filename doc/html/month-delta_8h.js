497c5ce9e21f1bb9b9152d348ebabbd93735a6a4
travis <travis>
2016-08-31 23:47:58 +0000

    Travis doc commit

--- doc/html/month-delta_8h.js
+++ doc/html/month-delta_8h.js
@@ -1,21 +1,21 @@
 var month-delta_8h =
 [
     [ "PresentMonthDeltaError", "month-delta_8h.html#aee28c4c9b4054448713e954a7944cb51", null ],
-    [ "MonthDelta_add_month_delta", "month-delta_8h.html#a9f725b1f782ab8ddd1aa734d2cdbce27", null ],
-    [ "MonthDelta_divide_by", "month-delta_8h.html#acd94c6f51a37b55c6e6b6ebd5a39cf7e", null ],
-    [ "MonthDelta_equal", "month-delta_8h.html#a346d0c310255f1f720506a15446497a3", null ],
     [ "MonthDelta_from_months", "month-delta_8h.html#ab2435ae92655bed23406d37b4b8f4750", null ],
     [ "MonthDelta_from_years", "month-delta_8h.html#aec5607ad9e7e090c39b2f50a68974ef0", null ],
+    [ "MonthDelta_zero", "month-delta_8h.html#aca81ef97d8ec1cd052ab391636f16bf7", null ],
     [ "MonthDelta_get_months", "month-delta_8h.html#aa01811194746354457e2b5e7a70dd59d", null ],
     [ "MonthDelta_get_years", "month-delta_8h.html#a37279655ebb7be159cdf2fcfbf460348", null ],
     [ "MonthDelta_get_years_decimal", "month-delta_8h.html#afa5436ebc73fc5ce02c99f86a05d377c", null ],
-    [ "MonthDelta_greater_than", "month-delta_8h.html#a87f541ed67c5071e5bf359f08a0c4413", null ],
-    [ "MonthDelta_greater_than_or_equal", "month-delta_8h.html#ac025396048e81af1dc75bc21b7a7c3a8", null ],
     [ "MonthDelta_is_negative", "month-delta_8h.html#a03c4b1b8f6edca4c798b469e3f03bdfa", null ],
+    [ "MonthDelta_negate", "month-delta_8h.html#a4d77c910d4ec5841e5e3f59d63490078", null ],
+    [ "MonthDelta_multiply_by", "month-delta_8h.html#a147987d17fd1c580240979fde395b723", null ],
+    [ "MonthDelta_divide_by", "month-delta_8h.html#acd94c6f51a37b55c6e6b6ebd5a39cf7e", null ],
+    [ "MonthDelta_add_month_delta", "month-delta_8h.html#a9f725b1f782ab8ddd1aa734d2cdbce27", null ],
+    [ "MonthDelta_subtract_month_delta", "month-delta_8h.html#aba1962383fdb09d60f24b863ee738982", null ],
+    [ "MonthDelta_equal", "month-delta_8h.html#a346d0c310255f1f720506a15446497a3", null ],
     [ "MonthDelta_less_than", "month-delta_8h.html#ac38bd594e6541d7943021c7fcf337c9d", null ],
     [ "MonthDelta_less_than_or_equal", "month-delta_8h.html#a6aa021d2410c395010cb579eb49249db", null ],
-    [ "MonthDelta_multiply_by", "month-delta_8h.html#a147987d17fd1c580240979fde395b723", null ],
-    [ "MonthDelta_negate", "month-delta_8h.html#a4d77c910d4ec5841e5e3f59d63490078", null ],
-    [ "MonthDelta_subtract_month_delta", "month-delta_8h.html#aba1962383fdb09d60f24b863ee738982", null ],
-    [ "MonthDelta_zero", "month-delta_8h.html#aca81ef97d8ec1cd052ab391636f16bf7", null ]
+    [ "MonthDelta_greater_than", "month-delta_8h.html#a87f541ed67c5071e5bf359f08a0c4413", null ],
+    [ "MonthDelta_greater_than_or_equal", "month-delta_8h.html#ac025396048e81af1dc75bc21b7a7c3a8", null ]
 ];
